43c96cb24a26173789e0b0da9eaa33eb36c2b421
raykkun <raiwijaya2016@gmail.com>
2025-12-29 07:12:48 +0700

    update enemy go to player and kill player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed: float = 50
 var player: CharacterBody2D = null
+@onready var snake_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	var players = get_tree().get_nodes_in_group("PlayerGroup")
@@ -12,8 +13,14 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if player != null:
 		var direction = global_position.direction_to(player.global_position)
+		print(direction)
+		#if direction > 0:
+			#snake_sprite.flip_h = false
+		#elif direction < 0:
+			#snake_sprite.flip_h = true
+			
 		velocity.x = direction.x * speed 
-		#velocity.y = 0
+		velocity.y = 0
 		move_and_slide()
 	
 	
