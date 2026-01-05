1d3fa2dead01ed022ea6a86482fec455ff675999
raykkun <raiwijaya2016@gmail.com>
2026-01-05 11:04:49 +0700

    update jump force when player died

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,14 +2,17 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
+@export var jump_force := -200 
+
 func _on_body_entered(body: Node2D) -> void:
-	print("die")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	if body.has_method("die"):
+		print("die")
+		Engine.time_scale = 0.5
+		body.die(jump_force)	
+		timer.start()
 	
 
 func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
 	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
  
