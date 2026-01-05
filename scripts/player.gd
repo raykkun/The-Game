ffb26754bc79e78eb6b97f871673a051b74d05e3
raykkun <raiwijaya2016@gmail.com>
2026-01-05 11:06:27 +0700

    update , add new function die, and variavle is_dead

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+var is_dead := false
 
 func _physics_process(delta: float) -> void:
+	# when player died can't move or process input keyboard noting
+	if is_dead:
+		velocity += get_gravity() * delta
+		move_and_slide()
+		return
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -32,3 +37,17 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
+
+# Function call on killxone script
+func die(jump_force := -200):
+	if is_dead:
+		return
+		
+	is_dead = true
+	velocity.x = 0
+	velocity.y = jump_force
+	animated_sprite.play("idle") # change if have animation death
+	
+	#disable collision
+	if has_node("CollisionShape2D"):
+		$CollisionShape2D.set_deferred("disabled", true)
